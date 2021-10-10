3fe1217707da9e012e82727fecfcccc0da8d9e3b
Jesse Schalken <me@jesseschalken.com>
2021-10-11 10:00:35 +1100

    Simplify files

--- test2/blah/main.test.ts
+++ test2/blah/main.test.ts
@@ -1,9 +1,5 @@
-describe("A suite is just a function", function () {
-    var a;
-    it("and so is a spec", function () {
-        a = true;
-        expect(a).toBe(true);
+describe("a", function () {
+    it("b", function () {
+        console.log("hello");
     });
 });
-
-
